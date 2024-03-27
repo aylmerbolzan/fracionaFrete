ee10c320e9888d47381ea75521874a7af5297bed
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-03-27 10:22:58 -0300

    feat: inclusão de download do relatório

--- script.js
+++ script.js
@@ -40,50 +40,82 @@ function adicionarNovoItem() {
 }
 
 function criarResumoItens() {
-  const modalAnterior = document.getElementById('resumoModal');
-  if (modalAnterior) {
-      modalAnterior.parentNode.removeChild(modalAnterior);
-  }
+    const modalAnterior = document.getElementById('resumoModal');
+    if (modalAnterior) {
+        modalAnterior.parentNode.removeChild(modalAnterior);
+    }
 
-  let resumoHTML = '<div class="modal fade" id="resumoModal" tabindex="-1" role="dialog" aria-labelledby="resumoModalLabel" aria-hidden="true">';
-  resumoHTML += '<div class="modal-dialog" role="document">';
-  resumoHTML += '<div class="modal-content">';
-  resumoHTML += '<div class="modal-header">';
-  resumoHTML += '<h5 class="modal-title" id="resumoModalLabel">Resumo dos Itens</h5>';
-  resumoHTML += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
-  resumoHTML += '<span aria-hidden="true">&times;</span>';
-  resumoHTML += '</button>';
-  resumoHTML += '</div>';
-  resumoHTML += '<div class="modal-body">';
-  resumoHTML += '<ul>';
+    let resumoHTML = '<div class="modal fade" id="resumoModal" tabindex="-1" role="dialog" aria-labelledby="resumoModalLabel" aria-hidden="true">';
+    resumoHTML += '<div class="modal-dialog" role="document">';
+    resumoHTML += '<div class="modal-content">';
+    resumoHTML += '<div class="modal-header">';
+    resumoHTML += '<h5 class="modal-title" id="resumoModalLabel">Resumo dos Itens</h5>';
+    resumoHTML += '<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
+    resumoHTML += '<span aria-hidden="true">&times;</span>';
+    resumoHTML += '</button>';
+    resumoHTML += '</div>';
+    resumoHTML += '<div class="modal-body">';
+    resumoHTML += '<ul>';
 
-  for (let i = 1; i <= itemCounter; i++) {
-      const quantidadeItem = parseInt(document.getElementById('quantidade_item' + i).value);
-      const alturaItem = parseFloat(document.getElementById('altura_item' + i).value);
-      const larguraItem = parseFloat(document.getElementById('largura_item' + i).value);
-      const comprimentoItem = parseFloat(document.getElementById('comprimento_item' + i).value);
-      const pesoItem = parseFloat(document.getElementById('peso_item' + i).value);
-      const freteItem = document.getElementById('frete_item' + i).value;
+    for (let i = 1; i <= itemCounter; i++) {
+        const quantidadeItem = parseInt(document.getElementById('quantidade_item' + i).value);
+        const alturaItem = parseFloat(document.getElementById('altura_item' + i).value);
+        const larguraItem = parseFloat(document.getElementById('largura_item' + i).value);
+        const comprimentoItem = parseFloat(document.getElementById('comprimento_item' + i).value);
+        const pesoItem = parseFloat(document.getElementById('peso_item' + i).value);
+        const freteItem = document.getElementById('frete_item' + i).value;
 
-      resumoHTML += `<li><strong>Item ${i}:<br></strong> ${quantidadeItem} uni - ${alturaItem} x ${larguraItem} x ${comprimentoItem} cm - ${pesoItem} kg: ${freteItem}</li><br>`;
-  }
+        resumoHTML += `<li><strong>Item ${i}:<br></strong> ${quantidadeItem} uni - ${alturaItem} x ${larguraItem} x ${comprimentoItem} cm - ${pesoItem} kg: ${freteItem}</li><br>`;
+    }
 
-  resumoHTML += '</ul>';
-  resumoHTML += '</div>';
-  resumoHTML += '<div class="modal-footer">';
-  resumoHTML += '<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>';
-  resumoHTML += '</div>';
-  resumoHTML += '</div>';
-  resumoHTML += '</div>';
-  resumoHTML += '</div>';
+    resumoHTML += '</ul>';
+    resumoHTML += '</div>';
+    resumoHTML += '<div class="modal-footer">';
+    resumoHTML += '<button type="button" class="btn btn-primary" id="btnDownloadResumo">Baixar Resumo</button>';
+    resumoHTML += '<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>';
+    resumoHTML += '</div>';
+    resumoHTML += '</div>';
+    resumoHTML += '</div>';
+    resumoHTML += '</div>';
 
-  const modalContainer = document.createElement('div');
-  modalContainer.innerHTML = resumoHTML;
-  document.body.appendChild(modalContainer);
+    const modalContainer = document.createElement('div');
+    modalContainer.innerHTML = resumoHTML;
+    document.body.appendChild(modalContainer);
 
-  $('#resumoModal').modal('show');
+    $('#resumoModal').modal('show');
+
+    document.getElementById('btnDownloadResumo').addEventListener('click', downloadResumo);
 }
 
+function downloadResumo() {
+    const resumo = gerarResumoTexto();
+
+    const blob = new Blob([resumo], { type: 'text/plain' });
+
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'resumo_itens.txt';
+    link.click();
+}
+
+function gerarResumoTexto() {
+    let resumo = '';
+    for (let i = 1; i <= itemCounter; i++) {
+        const quantidadeItem = parseInt(document.getElementById('quantidade_item' + i).value);
+        const alturaItem = parseFloat(document.getElementById('altura_item' + i).value);
+        const larguraItem = parseFloat(document.getElementById('largura_item' + i).value);
+        const comprimentoItem = parseFloat(document.getElementById('comprimento_item' + i).value);
+        const pesoItem = parseFloat(document.getElementById('peso_item' + i).value);
+        const freteItem = document.getElementById('frete_item' + i).value;
+
+        resumo += `Item ${i}:\n`;
+        resumo += `Quantidade: ${quantidadeItem} uni\n`;
+        resumo += `Dimensões: ${alturaItem} x ${larguraItem} x ${comprimentoItem} cm\n`;
+        resumo += `Peso: ${pesoItem} kg\n`;
+        resumo += `Valor do frete: ${freteItem}\n\n`;
+    }
+    return resumo;
+}
 
 function calcular() {
     var alturaPacote = parseFloat(document.getElementById('altura').value);
